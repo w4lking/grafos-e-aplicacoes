2ea058b6519e53f555dab5038bfd01d2e41be9d6
Layon Walker <layon.pedro@estudante.ufla.br>
2025-04-11 18:23:00 -0300

    adendos no main

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from leitura import parse_dat_file
 from estatisticas import calcular_estatisticas_sem_networkx
 from grafo import gerar_grafo_como_lista_adjacencia, desenhar_grafo_no_notebook
 
+
+# Função principal que percorre os arquivos .dat na pasta "selected_instances"
+# Obs: ao rodar o main ele vai ler todos os arquivos .dat que estão na pasta "selected_instances" e gerar os grafo correspondentes a cada arquivo .dat.
+# Por tanto é importante rodar no notebook se não quiser que leia 50 arquivos .dat de uma vez só
 def main():
     pasta = "selected_instances"
 
